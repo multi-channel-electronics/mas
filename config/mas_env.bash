57769bac6093b7d04195284f1fecd6724c2e16f0
mhasse <mhasse@c56f3402-693b-0410-897c-fe4ccea3857b>
2008-02-12 14:20:12 +0000

    Working mce config file for clover.

--- config/mas_env.bash
+++ config/mas_env.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ "$MAS_ROOT" == "" ]; then
-	export MAS_ROOT=/home/mce/
+	export MAS_ROOT=/home/mce/mce_script/
 fi
 
 #Trailing slashes are mandatory!!
 
 export MAS_BIN=/usr/mce/bin/
 export MAS_DATA=/data/cryo/current_data/
-export MAS_TEMPLATE=${MAS_ROOT}/mce_script/clover/template/
-export MAS_SCRIPT=${MAS_ROOT}/mce_script/clover/script/
-export MAS_TEMP=${MAS_ROOT}/mce_script/tmp/
+export MAS_TEMPLATE=${MAS_ROOT}template/
+export MAS_SCRIPT=${MAS_ROOT}script/
+export MAS_TEMP=/home/mce/tmp/
 export MAS_IDL=${MAS_ROOT}idl_pro/
 export PATH=${PATH}:${MAS_BIN}:${MAS_SCRIPT}
