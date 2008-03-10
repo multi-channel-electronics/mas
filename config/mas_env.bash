645d218740e9675ea0e7c6a38efb3abe30c67db8
mce <mce@c56f3402-693b-0410-897c-fe4ccea3857b>
2008-03-10 21:51:54 +0000

    Re-vamped mas_env.bash for having mce_script  in /usr/mce.  Following this, I will remove mas_env.bash and put it into mce_script/template.

--- config/mas_env.bash
+++ config/mas_env.bash
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+# To run the mce_scripts from your own checked-out source tree, point
+# the MAS_ROOT variable to your checked out copy and then source this
+# file.  e.g.:
+#
+#     export MAS_ROOT="/home/mhasse/mce_script/trunk/"
+#     source mas_env.bash
+#
+# If your .bashrc sources mas_env.bash automatically, with a different
+# MAS_ROOT, you probably need to set the $PATH variable by hand as it
+# will still preferentially run things from the old directories.
+
 if [ "$MAS_ROOT" == "" ]; then
-	export MAS_ROOT=/home/mce/
+	export MAS_ROOT=/usr/mce/mce_script
 fi
 
-#Trailing slashes are mandatory!!
+#Trailing slashes are recommended
 
 export MAS_BIN=/usr/mce/bin/
+export MAS_TEMP=/tmp/
 export MAS_DATA=/data/cryo/current_data/
-export MAS_TEMPLATE=${MAS_ROOT}template/
-export MAS_SCRIPT=${MAS_ROOT}script/
-export MAS_TEMP=${MAS_ROOT}tmp/
-export MAS_IDL=${MAS_ROOT}idl_pro/
-export PATH=${PATH}:${MAS_BIN}:${MAS_SCRIPT}
+
+export MAS_TEMPLATE=${MAS_ROOT}/template/
+export MAS_SCRIPT=${MAS_ROOT}/script/
+export MAS_TEST_SUITE=${MAS_ROOT}/test_suite/
+export MAS_IDL=${MAS_ROOT}/idl_pro/
+
+export PATH=${PATH}:${MAS_BIN}:${MAS_SCRIPT}:${MAS_TEST_SUITE}
