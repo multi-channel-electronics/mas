32f38c489bfda00db756706a855ffb736c28fa25
mhasse <mhasse@c56f3402-693b-0410-897c-fe4ccea3857b>
2011-06-29 02:03:03 +0000

    Ethernet connection code.

--- interfaces/mce_library/eth.c
+++ interfaces/mce_library/eth.c
@@ -4,15 +4,73 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include <mce_library.h>
 #include "context.h"
 
 #include "eth.h"
 
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+//#include <net/if.h>
+
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+#include <sys/ioctl.h>
+
+/* Our private data */
+
+typedef struct {
+    int socket;
+    struct sockaddr source_mac;
+    struct sockaddr dest_mac;
+} eth_transport;
+
+
+
 /* CMD subsystem */
 int mcecmd_eth_connect(mce_context_t context)
 {
+    int err = -1;
+    struct ifreq ifr;
+
+    // Allocate private data
+    context->transport_data = NULL;
+    eth_transport *transport = malloc(sizeof(*transport));
+    
+    // Create socket for raw packet transfers
+    transport->socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+
+    // Determine network interface number for our device (dev_name)
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy((char*)ifr.ifr_name, context->dev_name, IFNAMSIZ);
+    err = ioctl(transport->socket, SIOCGIFINDEX, &ifr);
+    if (err < 0) {
+        fprintf(stderr, "Failed to determine ethernet interface number, (%i).\n", errno);
+        abort();
+    }
+
+    // Determine mac address of our network interface
+    err = ioctl(transport->socket, SIOCGIFHWADDR, &ifr);
+    if (err < 0) {
+        fprintf(stderr, "Failed to determine ethernet interface MAC address, (%i).\n", errno);
+        abort();
+    }
+    memcpy(&transport->source_mac, &ifr.ifr_hwaddr, sizeof(ifr.ifr_hwaddr));
+
+    // Also store target mac address, which has been encoded as a uint64_t
+    int i;
+    for (i=0; i<sizeof(context->dev_num); i++)
+        transport->dest_mac.sa_data[i] = (context->dev_num >> (8*i)) & 0xff;
+
+    // Is that good enough?
+    context->transport_data = transport;
+
     fprintf(stderr, "Some work is needed on line %i of %s\n", __LINE__,
             __FILE__);
     abort();
