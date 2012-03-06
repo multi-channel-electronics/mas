4b274cecd2ed202a041cf6f2658c5084503ef285
mhasse <mhasse@c56f3402-693b-0410-897c-fe4ccea3857b>
2012-03-06 03:40:08 +0000

    Move to distutils build system.

--- python/mcelib/setup.py
+++ python/mcelib/setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('mce',
-                    sources = ['pymce.c'])
+module1 = Extension('mcelib',
+                    sources = ['base.c'],
+                    include_dirs=['/usr/mce/include'],
+                    library_dirs=['/usr/mce/lib'],
+                    libraries=['mcecmd', 'mcedsp', 'maslog', 'config'])
 
-setup (name = 'pymce',
+setup (name = 'mcelib',
        version = '1.0',
        description = 'Interface to the UBC MCE electronics.',
        ext_modules = [module1])
