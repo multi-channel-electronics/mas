58905e7325d1929ba17426bb3cacb6dfb096f951
mhasse <mhasse@c56f3402-693b-0410-897c-fe4ccea3857b>
2014-04-14 18:56:30 +0000

    Clean up dsp_driver.h.

--- driver/dsp_driver.h
+++ driver/dsp_driver.h
@@ -11,13 +11,6 @@
 #define DSPDEV_NAME "mce_dsp"
 #define DSP_DEFAULT_TIMEOUT (HZ*500/1000)
 
-/* DSP code versions */
-
-#define DSP_U0103          0x550103
-#define DSP_U0104          0x550104
-#define DSP_U0105          0x550105
-#define DSP_U0106          0x550106
-
 
 /* DSP PCI vendor/device id */
 
@@ -33,18 +26,8 @@
 #define HCVR_MCE_RST     (HCVR_HNMI | 0x008D) /* Reset MCE comms circuits */
 
 /* HCTR default */
-
 #define DSP_PCI_MODE_BASE        0x000900    /* for 32->24 bit conversion */
-#define DSP_PCI_MODE_HANDSHAKE   HCTR_HF1
-#define DSP_PCI_MODE_NOIRQ       HCTR_HF2
 
-
-#define PCI_MAX_FLUSH       256
-
-/* Soft interrupt generation timer frequency */
-
-#define DSP_POLL_FREQ       100
-#define DSP_POLL_JIFFIES    (HZ / DSP_POLL_FREQ + 1)
 
 typedef struct {
 	volatile
@@ -101,9 +84,5 @@ typedef struct {
         int force_exit;
 
 } frame_buffer_t;
-
-#define DSP_INTERNAL_FREQ  50e6
-#define DSP_INFORM_RATE    10 /* Hz */
-#define DSP_INFORM_COUNTS  (DSP_INTERNAL_FREQ / DSP_INFORM_RATE) 
 
 #endif
